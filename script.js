0f1bd76949df0410a634c2bee3afb8855e1be483
Simmi Badhan <simmi@addvalsolutions.com>
2023-07-10 11:59:49 +0530

    Iallo chat testing - port 3012

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const socket = io('http://localhost:3000')
+const socket = io('ws://localhost:3012')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
-
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+const chatId = prompt('What is your chat id?')
+// appendMessage('You joined')
+socket.emit('chat-joined', chatId)
 
 socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
+  appendMessage(`Welcome ${name} !`)
 })
 
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+// Send chat message
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
+  // appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', { chat_id: chatId, message: message })
   messageInput.value = ''
 })
 
